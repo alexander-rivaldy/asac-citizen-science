edb91f215bb94c4c4aaa6307e71d0f4552804273
s3466521 <s3466521@student.rmit.edu.au>
2017-08-13 02:59:21 +0000

    initialize home page, layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    
+    def full_title(title)
+        base = "The Citizen Science Project"
+        if title.empty?
+            base
+        else
+            title + " | " + base
+        end
+    end
+    
 end
